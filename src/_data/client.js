75422a10dde7da56741a90830350df7406d9558c
“andrewbramwell <andy@voucherstore.io>
2025-12-13 19:41:26 +0700

    schema

--- src/_data/client.js
+++ src/_data/client.js
@@ -20,7 +20,60 @@ module.exports = {
         "googleReview": "https://g.page/r/CY4v3czmGv6ZEBM/review",
     },
     //! Make sure you include the file protocol (e.g. https://) and that NO TRAILING SLASH is included
-    "domain": "https://www.regentthaimassageandspa.co.uk"
+    "domain": "https://www.regentthaimassageandspa.co.uk",
+    "schema": {
+        "business": {
+            "@context": "https://schema.org",
+            "@type": "HealthAndBeautyBusiness",
+            "@id": "https://www.regentthaimassageandspa.co.uk/#business",
+            "name": "Regent Thai Massage & Spa",
+            "image": "https://www.regentthaimassageandspa.co.uk/assets/svgs/logo-l.svg",
+            "url": "https://www.regentthaimassageandspa.co.uk",
+            "telephone": "+447301291923",
+            "email": "info@regentthaimassageandspa.co.uk",
+            "priceRange": "££",
+            "address": {
+                "@type": "PostalAddress",
+                "streetAddress": "3, Ormond Terrace, Regent St",
+                "addressLocality": "Cheltenham",
+                "postalCode": "GL50 1HR",
+                "addressCountry": "GB"
+            },
+            "geo": {
+                "@type": "GeoCoordinates",
+                "url": "https://maps.app.goo.gl/qAHseAEmAKRpXHxF6"
+            },
+            "openingHoursSpecification": [
+                {
+                    "@type": "OpeningHoursSpecification",
+                    "dayOfWeek": ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"],
+                    "opens": "10:00",
+                    "closes": "20:00"
+                },
+                {
+                    "@type": "OpeningHoursSpecification",
+                    "dayOfWeek": "Sunday",
+                    "opens": "11:00",
+                    "closes": "19:00"
+                }
+            ],
+            "sameAs": [
+                "https://www.facebook.com/people/Regent-Thai-massage-Cheltenham/61570157464844/?locale=en_GB",
+                "https://www.instagram.com/regent_thaimassage_cheltenham/",
+                "https://g.page/r/CY4v3czmGv6ZEBM/review"
+            ]
+        },
+        "website": {
+            "@context": "https://schema.org",
+            "@type": "WebSite",
+            "@id": "https://www.regentthaimassageandspa.co.uk/#website",
+            "url": "https://www.regentthaimassageandspa.co.uk",
+            "name": "Regent Thai Massage & Spa",
+            "publisher": {
+                "@id": "https://www.regentthaimassageandspa.co.uk/#business"
+            }
+        }
+    }
 };
 
 
